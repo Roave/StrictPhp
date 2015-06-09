b729a9776eef4536d7f973fd7d33ea597778ebb6
Marco Pivetta <ocramius@gmail.com>
2015-06-10 00:37:42 +0100

    Configuring/initializing aspect kernel

--- test/functional/init.php
+++ test/functional/init.php
@@ -1,3 +1,9 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
+
+\StrictPhp\StrictPhpKernel::getInstance()->init([
+    'debug'        => true,
+    'cacheDir'     => realpath(__DIR__ . '/../..') . '/go-cache/',
+    'includePaths' => [],
+]);
