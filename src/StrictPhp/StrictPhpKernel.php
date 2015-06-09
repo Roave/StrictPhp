ca183d195b2dda6f207b6d5d2b1b496bf0605924
Marco Pivetta <ocramius@gmail.com>
2015-06-10 00:17:52 +0100

    First aspect kernel for the StrictPhp project

---
+++ src/StrictPhp/StrictPhpKernel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace StrictPhp;
+
+use Go\Core\AspectContainer;
+use Go\Core\AspectKernel;
+
+class StrictPhpKernel extends AspectKernel
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureAop(AspectContainer $container)
+    {
+        // @TODO
+    }
+}
