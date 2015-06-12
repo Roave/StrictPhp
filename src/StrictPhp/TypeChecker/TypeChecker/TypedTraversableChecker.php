22a4214966124e512de03f287e2cf918ce2c4e71
Marco Pivetta <ocramius@gmail.com>
2015-06-12 19:19:03 +0100

    First draft of a typed traversable checker

---
+++ src/StrictPhp/TypeChecker/TypeChecker/TypedTraversableChecker.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace StrictPhp\TypeChecker\TypeChecker;
+
+use StrictPhp\TypeChecker\TypeCheckerInterface;
+
+final class TypedTraversableChecker implements TypeCheckerInterface
+{
+    /**
+     * @var TypeCheckerInterface[]
+     */
+    private $typeCheckers;
+
+    /**
+     * @param TypeCheckerInterface ...$typeCheckers
+     */
+    public function __construct(TypeCheckerInterface ...$typeCheckers)
+    {
+        /* @var $typeCheckers TypeCheckerInterface[] */
+        $this->typeCheckers = array_merge($typeCheckers, [$this]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function canApplyToType($type)
+    {
+        return substr($type, -2) === '[]'
+            && $this->getCheckersApplicableToType(substr($type, 0, -2));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validate($value, $type)
+    {
+        return substr($type, -2) === '[]'
+            && (($value instanceof \Traversable) || is_array($value))
+            && $this->getCheckersValidForType($value, substr($type, 0, -2));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function simulateFailure($value, $type)
+    {
+        array_map(
+            function (TypeCheckerInterface $typeChecker) use ($value, $type) {
+                $typeChecker->simulateFailure($value, $type);
+            },
+            $this->getCheckersApplicableToType($type)
+        );
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return TypeCheckerInterface[]
+     */
+    private function getCheckersApplicableToType($type)
+    {
+        return array_filter(
+            $this->typeCheckers,
+            function (TypeCheckerInterface $typeChecker) use ($type) {
+                return $typeChecker->canApplyToType($type);
+            }
+        );
+    }
+
+    /**
+     * @param mixed  $value
+     * @param string $type
+     *
+     * @return TypeCheckerInterface[]
+     */
+    private function getCheckersValidForType($value, $type)
+    {
+        return array_filter(
+            $this->typeCheckers,
+            function (TypeCheckerInterface $typeChecker) use ($value, $type) {
+                return $typeChecker->validate($value, $type);
+            }
+        );
+    }
+}
