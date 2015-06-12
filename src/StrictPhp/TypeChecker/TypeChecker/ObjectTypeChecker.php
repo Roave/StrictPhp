360921c64e6ac36c26b8f552bfc99d0611a63bac
Marco Pivetta <ocramius@gmail.com>
2015-06-12 18:03:30 +0100

    Implemented object type checker

---
+++ src/StrictPhp/TypeChecker/TypeChecker/ObjectTypeChecker.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace StrictPhp\TypeChecker\TypeChecker;
+
+use StrictPhp\TypeChecker\TypeCheckerInterface;
+
+final class bjectTypeChecker implements TypeCheckerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function canApplyToType($type)
+    {
+        return class_exists($type);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validate($value, $type)
+    {
+        return $value instanceof $type;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function simulateFailure($value, $type)
+    {
+        if (! $this->canApplyToType($type)) {
+            throw new \InvalidArgumentException(sprintf('The provided type "%s" is not a valid class', $type));
+        }
+
+        /* @var $callback callable */
+        $callback = eval(sprintf('return function (%s $value) {};'));
+
+        $callback($value);
+    }
+}
