b440bd414d86dc20b44508b7bcbbf16f8b855ede
Marco Pivetta <ocramius@gmail.com>
2015-06-12 17:10:18 +0100

    Type checker interface

---
+++ src/StrictPhp/TypeChecker/TypeCheckerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace StrictPhp\TypeChecker;
+
+interface TypeCheckerInterface
+{
+    /**
+     * @param string $type
+     *
+     * @return bool
+     */
+    public function canApplyToType($type);
+
+    /**
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function validate($value);
+
+    /**
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function simulateFailure($value);
+}
