bc22729b6bbaded74defa5a37403f2886589ad66
Marco Pivetta <ocramius@gmail.com>
2015-06-14 21:08:33 +0200

    Using PHPDocumentor's type resolver to resolve types

--- src/StrictPhp/TypeFinder/PropertyTypeFinder.php
+++ src/StrictPhp/TypeFinder/PropertyTypeFinder.php
@@ -4,6 +4,8 @@ namespace StrictPhp\TypeFinder;
 
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlock\Tag\VarTag;
+use phpDocumentor\Reflection\TypeResolver;
+use phpDocumentor\Reflection\Types\ContextFactory;
 use ReflectionProperty;
 
 final class PropertyTypeFinder
@@ -11,20 +13,28 @@ final class PropertyTypeFinder
     /**
      * @param ReflectionProperty $reflectionProperty
      *
-     * @return string[]
+     * @return \phpDocumentor\Reflection\Type[]
      */
     public function __invoke(ReflectionProperty $reflectionProperty)
     {
-        return array_unique(array_merge(
+        $typeResolver   = new TypeResolver();
+        $context        = (new ContextFactory())->createFromReflector($reflectionProperty->getDeclaringClass());
+
+        return array_unique(array_filter(array_merge(
             [],
             [],
             ...array_map(
-                function (VarTag $varTag) {
-                    return $varTag->getTypes();
+                function (VarTag $varTag) use ($typeResolver, $context) {
+                    return array_map(
+                        function ($type) use ($typeResolver, $context) {
+                            return $typeResolver->resolve($type, $context);
+                        },
+                        $varTag->getTypes()
+                    );
                 },
                 // note: this will not compute imports for us! Need a better alternative!
                 (new DocBlock($reflectionProperty))->getTagsByName('var')
             )
-        ));
+        )));
     }
 }
