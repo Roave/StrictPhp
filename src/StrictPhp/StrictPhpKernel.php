9c4bc8edfe8f01620f82dbb53f215b3aedb51570
Marco Pivetta <ocramius@gmail.com>
2015-06-27 04:36:49 +0200

    Reusing simpler checker components in the strict PHP kernel initialization

--- src/StrictPhp/StrictPhpKernel.php
+++ src/StrictPhp/StrictPhpKernel.php
@@ -4,10 +4,20 @@ namespace StrictPhp;
 
 use Go\Core\AspectContainer;
 use Go\Core\AspectKernel;
+use StrictPhp\AccessChecker\ObjectStateChecker;
 use StrictPhp\AccessChecker\PropertyWriteImmutabilityChecker;
 use StrictPhp\AccessChecker\PropertyWriteTypeChecker;
-use StrictPhp\Aspect\PostConstructStateCheck;
+use StrictPhp\Aspect\PostConstructAspect;
 use StrictPhp\Aspect\PropertyWriteAspect;
+use StrictPhp\TypeChecker\ApplyTypeChecks;
+use StrictPhp\TypeChecker\TypeChecker\CallableTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\GenericObjectTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\IntegerTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\MixedTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\ObjectTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\StringTypeChecker;
+use StrictPhp\TypeChecker\TypeChecker\TypedTraversableChecker;
+use StrictPhp\TypeFinder\PropertyTypeFinder;
 
 class StrictPhpKernel extends AspectKernel
 {
@@ -16,10 +26,24 @@ class StrictPhpKernel extends AspectKernel
      */
     protected function configureAop(AspectContainer $container)
     {
-        $container->registerAspect(new PostConstructStateCheck());
+        $typeCheckers = [
+            new IntegerTypeChecker(),
+            new CallableTypeChecker(),
+            new StringTypeChecker(),
+            new GenericObjectTypeChecker(),
+            new ObjectTypeChecker(),
+            new MixedTypeChecker(),
+        ];
+
+        $typeCheckers[] = new TypedTraversableChecker(...$typeCheckers);
+
         $container->registerAspect(new PropertyWriteAspect(
             new PropertyWriteImmutabilityChecker(),
             new PropertyWriteTypeChecker()
         ));
+        $container->registerAspect(new PostConstructAspect(new ObjectStateChecker(
+            new ApplyTypeChecks(...$typeCheckers),
+            new PropertyTypeFinder()
+        )));
     }
 }
