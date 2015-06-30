8c94f5a3857dacd8711cc84ba97b58db98fd182e
Marco Pivetta <ocramius@gmail.com>
2015-06-30 07:07:18 +0100

    Removing duplicate code

--- src/StrictPhp/AccessChecker/ParameterTypeChecker.php
+++ src/StrictPhp/AccessChecker/ParameterTypeChecker.php
@@ -21,15 +21,7 @@ namespace StrictPhp\AccessChecker;
 use Go\Aop\Intercept\MethodInvocation;
 use Go\Lang\Annotation as Go;
 use phpDocumentor\Reflection\DocBlock;
-use phpDocumentor\Reflection\DocBlock\Context;
-use phpDocumentor\Reflection\Fqsen;
 use phpDocumentor\Reflection\Type;
-use phpDocumentor\Reflection\TypeResolver;
-use phpDocumentor\Reflection\Types\ContextFactory;
-use phpDocumentor\Reflection\Types\Object_;
-use phpDocumentor\Reflection\Types\Self_;
-use phpDocumentor\Reflection\Types\Static_;
-use ReflectionMethod;
 use ReflectionParameter;
 use StrictPhp\TypeChecker\ApplyTypeChecks;
 use StrictPhp\TypeChecker\TypeChecker\CallableTypeChecker;
@@ -40,6 +32,7 @@ use StrictPhp\TypeChecker\TypeChecker\NullTypeChecker;
 use StrictPhp\TypeChecker\TypeChecker\ObjectTypeChecker;
 use StrictPhp\TypeChecker\TypeChecker\StringTypeChecker;
 use StrictPhp\TypeChecker\TypeChecker\TypedTraversableChecker;
+use StrictPhp\TypeFinder\ParameterTypeFinder;
 
 final class ParameterTypeChecker
 {
@@ -52,16 +45,12 @@ final class ParameterTypeChecker
      */
     public function __invoke(MethodInvocation $methodInvocation)
     {
-        $method               = $methodInvocation->getMethod();
-        $paramTags            = $this->getParamTags($method);
-        $reflectionParameters = $method->getParameters();
+        $reflectionParameters = $methodInvocation->getMethod()->getParameters();
 
         foreach ($methodInvocation->getArguments() as $argumentIndex => $argument) {
             $this->applyTypeChecks(
                 $this->getParameterDocblockType(
-                    $method,
                     get_class($methodInvocation->getThis()),
-                    $paramTags,
                     $reflectionParameters,
                     $argumentIndex
                 ),
@@ -97,15 +86,13 @@ final class ParameterTypeChecker
     }
 
     /**
-     * @param ReflectionMethod        $method
      * @param string                  $contextClass
-     * @param DocBlock\Tag\ParamTag[] $paramTags
      * @param \ReflectionParameter[]  $reflectionParameters
      * @param int                     $index
      *
      * @return Type[]
      */
-    private function getParameterDocblockType(ReflectionMethod $method, $contextClass, array $paramTags, array $reflectionParameters, $index)
+    private function getParameterDocblockType($contextClass, array $reflectionParameters, $index)
     {
         // @TODO consider variadic arguments here
 
@@ -113,99 +100,7 @@ final class ParameterTypeChecker
             return [];
         }
 
-        if (! $paramTag = $this->getParamTagByName($paramTags, $reflectionParameters[$index]->getName())) {
-            return [];
-        }
-
-        return $this->expandParameterTypes($paramTag, $method, $contextClass);
-    }
-
-    /**
-     * @param DocBlock\Tag\ParamTag $param
-     * @param ReflectionMethod      $reflectionMethod
-     * @param string                $contextClass
-     *
-     * @return Type[]
-     */
-    private function expandParameterTypes(DocBlock\Tag\ParamTag $param, ReflectionMethod $reflectionMethod, $contextClass)
-    {
-        $typeResolver = new TypeResolver();
-        $context      = (new ContextFactory())->createFromReflector($reflectionMethod);
-
-        return array_map(
-            function (Type $type) use ($reflectionMethod, $contextClass) {
-                return $this->expandSelfAndStaticTypes($type, $reflectionMethod, $contextClass);
-            },
-            array_unique(array_filter(array_merge(
-                [],
-                [],
-                ...array_map(
-                    function (DocBlock\Tag\ParamTag $paramTag) use ($typeResolver, $context) {
-                        return array_map(
-                            function ($type) use ($typeResolver, $context) {
-                                return $typeResolver->resolve($type, $context);
-                            },
-                            $paramTag->getTypes()
-                        );
-                    },
-                    [$param] // @TODO could be more than one @param
-                )
-            )))
-        );
-    }
-
-    /**
-     * Replaces "self", "$this" and "static" types with the corresponding runtime versions
-     *
-     * @todo may be removed if PHPDocumentor provides a runtime version of the types VO
-     *
-     * @param Type             $type
-     * @param ReflectionMethod $reflectionMethod
-     * @param string           $contextClass
-     *
-     * @return Type
-     */
-    private function expandSelfAndStaticTypes(Type $type, ReflectionMethod $reflectionMethod, $contextClass)
-    {
-        if ($type instanceof Self_) {
-            return new Object_(new Fqsen('\\' . $reflectionMethod->getDeclaringClass()->getName()));
-        }
-
-        if ($type instanceof Static_) {
-            return new Object_(new Fqsen('\\' . $contextClass));
-        }
-
-        return $type;
-    }
-
-    /**
-     * @param DocBlock\Tag\ParamTag[] $paramTags
-     * @param string                  $name
-     *
-     * @return DocBlock\Tag\ParamTag|null
-     */
-    private function getParamTagByName(array $paramTags, $name)
-    {
-        foreach ($paramTags as $paramTag) {
-            if (ltrim($paramTag->getVariableName(), '$') === $name) {
-                return $paramTag;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @param ReflectionMethod $reflectionMethod
-     *
-     * @return DocBlock\Tag\ParamTag[]
-     */
-    private function getParamTags(ReflectionMethod $reflectionMethod)
-    {
-        return (new DocBlock(
-            $reflectionMethod,
-            new Context($reflectionMethod->getDeclaringClass()->getNamespaceName())
-        ))
-            ->getTagsByName('param');
+        return (new ParameterTypeFinder())
+            ->__invoke($reflectionParameters[$index], $contextClass);
     }
 }
