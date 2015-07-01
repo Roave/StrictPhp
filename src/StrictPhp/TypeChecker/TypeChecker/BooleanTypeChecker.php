a1843b3ed8139963a3afb1e325a948e426d7d696
Marco Pivetta <ocramius@gmail.com>
2015-07-01 02:16:36 +0100

    Implementing boolean type checker

---
+++ src/StrictPhp/TypeChecker/TypeChecker/BooleanTypeChecker.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace StrictPhp\TypeChecker\TypeChecker;
+
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Boolean;
+use phpDocumentor\Reflection\Types\Integer;
+use StrictPhp\TypeChecker\TypeCheckerInterface;
+
+final class BooleanTypeChecker implements TypeCheckerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function canApplyToType(Type $type)
+    {
+        return $type instanceof Boolean;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function validate($value, Type $type)
+    {
+        if (! $type instanceof Boolean) {
+            throw new \InvalidArgumentException(sprintf(
+                'Unsupported type "%s" given, expecting "%s"',
+                get_class($type),
+                Boolean::class
+            ));
+        }
+
+        return is_bool($value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function simulateFailure($value, Type $type)
+    {
+        if (! $this->validate($value, $type)) {
+            // @TODO bump to PHP 7 and use strict scalar types + a closure.
+            throw new \ErrorException('NOPE');
+        }
+    }
+}
