7c6b5f8be1cbeada54200cfec9d2c4ed8a5c39f0
Marco Pivetta <ocramius@gmail.com>
2015-07-29 13:45:48 +0200

    Correcting cache dir path (always to be provided)

--- tests/StrictPhpTest/StrictPhpKernelTest.php
+++ tests/StrictPhpTest/StrictPhpKernelTest.php
@@ -59,7 +59,7 @@ class StrictPhpKernelTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->baseConfig = [
-            //'cacheDir' => realpath(__DIR__ . '/..') . '/integration-tests-go-cache/',
+            'cacheDir' => realpath(__DIR__ . '/..') . '/integration-tests-go-cache/',
             'includePaths' => [__DIR__],
         ];
     }
