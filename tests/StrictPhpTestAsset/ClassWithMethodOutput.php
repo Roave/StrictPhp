742838f3cdc853dafda668767fe146b4489c641b
Jefersson Nathan <admin@phpse.net>
2015-09-21 19:41:39 -0300

    #51 - Add regression test

---
+++ tests/StrictPhpTestAsset/ClassWithMethodOutput.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: malukenho
+ * Date: 11/27/15
+ * Time: 7:32 PM
+ */
+
+namespace StrictPhpTestAsset;
+
+/**
+ * @uthor Jefersson Nathan <malukenho@phpse.net>
+ */
+class ClassWithMethodOutput
+{
+    public function hello()
+    {
+        echo 'Hello';
+    }
+}
